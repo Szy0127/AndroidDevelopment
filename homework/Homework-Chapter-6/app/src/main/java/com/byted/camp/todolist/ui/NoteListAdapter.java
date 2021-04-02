6eef0479217c494d3454ef03b98f142d76048358
Szy0127 <1799853523@qq.com>
2021-04-02 15:00:41 +0800

    c6

---
+++ homework/Homework-Chapter-6/app/src/main/java/com/byted/camp/todolist/ui/NoteListAdapter.java
@@ -0,0 +1,75 @@
+package com.byted.camp.todolist.ui;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+import com.byted.camp.todolist.MainActivity;
+import com.byted.camp.todolist.NoteActivity;
+import com.byted.camp.todolist.NoteOperator;
+import com.byted.camp.todolist.R;
+import com.byted.camp.todolist.beans.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
+
+public class NoteListAdapter extends RecyclerView.Adapter<NoteViewHolder> {
+
+    private final NoteOperator operator;
+    private final List<Note> notes = new ArrayList<>();
+
+    public NoteListAdapter(NoteOperator operator) {
+        this.operator = operator;
+    }
+
+    public void refresh(List<Note> newNotes) {
+        notes.clear();
+        if (newNotes != null) {
+            notes.addAll(newNotes);
+        }
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int pos) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_note, parent, false);
+        final NoteViewHolder holder = new NoteViewHolder(itemView, operator);
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener()
+        {
+            @Override
+            public boolean onLongClick(View v)
+            {
+                int position = holder.getAdapterPosition();
+                Note note = notes.get(position);
+
+                //Toast.makeText(v.getContext(),note.getContent(),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(v.getContext(), NoteActivity.class);
+                intent.putExtra("note", note);
+                v.getContext().startActivity(intent);
+                return true;
+            }
+        });
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int pos) {
+        holder.bind(notes.get(pos));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+}
